6097a272169f6cdc4f83b3a21d3079109f7b04c6
Phong Nguyen <phong204n@gmail.com>
2025-11-12 01:19:22 -0800

    testing

--- app/main.py
+++ app/main.py
@@ -14,21 +14,20 @@ def main(**kwargs):
     setLogLevel("info")
     net = Mininet(topo=tmp1, switch=OVSKernelSwitch, controller=DefaultController, autoSetMacs=True)
     net.start()
-    
-    # net["h1"].cmd("/usr/sbin/sshd")
-    # net["h2"].cmd("/usr/sbin/sshd")
-    # net["h3"].cmd("/usr/sbin/sshd")
 
     # net["h1"].cmd("ip route add default via 10.0.0.3")
     # net["h2"].cmd("ip route add default via 10.0.3.4")
     # net["h3"].cmd("ip route add default via 10.0.2.1")
 
-    net["r1"].cmd("ip route add to 10.0.0.0 via 10.0.0.1 dev r1-eth1")
-    net["r1"].cmd("ip route add to 10.0.3.0 via 10.0.3.2 dev r1-eth2")
-    net["r1"].cmd("ip route add to 10.0.1.0 via 10.0.1.2 dev r1-eth3")
+    # net["r1"].cmd("ip route add to 10.0.0.0 via 10.0.0.1 dev r1-eth1")
+    # net["r1"].cmd("ip route add to 10.0.3.0 via 10.0.3.2 dev r1-eth2")
 
+    net["r1"].cmd("ip route add to 10.0.1.0 via 10.0.1.2 dev r1-eth3")
     net["r2"].cmd("ip route add to 10.0.1.0 via 10.0.1.1 dev r2-eth1")
-    net["r2"].cmd("ip route add to 10.0.2.0 via 10.0.2.2 dev r2-eth2")
+
+    # net["r2"].cmd("ip route add to 10.0.2.0 via 10.0.2.2 dev r2-eth2")
+
+    net["r1"].cmd("ip route add to 10.0.2.0 via 10.0.1.2 dev r1-eth3")
 
     CLI(net)
     input("Network running. Press Enter to stop...")
