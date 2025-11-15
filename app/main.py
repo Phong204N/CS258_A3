fa8b15f93267298e58ee4133e2ae31595121655d
Phong Nguyen <phong204n@gmail.com>
2025-11-15 13:10:33 -0800

    ...

--- app/main.py
+++ app/main.py
@@ -7,22 +7,25 @@ from model.exp1 import exp1Topo
 from mininet.net import Mininet
 from mininet.node import Node, OVSKernelSwitch, Controller, RemoteController, DefaultController
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info
+from mininet.log import setLogLevel
 
 def main(**kwargs):
     tmp1:exp1Topo = exp1Topo()
-    setLogLevel("info")
+    setLogLevel("debug")
     net = Mininet(topo=tmp1, switch=OVSKernelSwitch, controller=DefaultController, autoSetMacs=True)
     # net = Mininet(topo=tmp1)
     net.start()
+    net["h1"].cmd('/usr/sbin/sshd')
+    net["h2"].cmd('/usr/sbin/sshd')
+    net["h3"].cmd('/usr/sbin/sshd')
 
     # net["h1"].cmd("ip route add default via 10.0.0.3")
     # net["h2"].cmd("ip route add default via 10.0.3.4")
     # net["h3"].cmd("ip route add default via 10.0.2.1")
 
-    net["r1"].cmd("ip route add 10.0.2.0/24 via 10.0.1.2 dev r1-eth3")
-    net["r2"].cmd("ip route add 10.0.0.0/24 via 10.0.1.1 dev r2-eth1")
-    net["r2"].cmd("ip route add 10.0.3.0/24 via 10.0.1.1 dev r2-eth1")
+    net["r1"].cmd("ip route add to 10.0.2.0/24 via 10.0.1.2 dev r1-eth3")
+    net["r2"].cmd("ip route add to 10.0.0.0/24 via 10.0.1.1 dev r2-eth1")
+    net["r2"].cmd("ip route add to 10.0.3.0/24 via 10.0.1.1 dev r2-eth1")
 
     CLI(net)
     input("Network running. Press Enter to stop...")
