db1645db43cba79067e90110e455338319ead57a
Phong Nguyen <phong204n@gmail.com>
2025-11-14 22:06:27 -0800

    ...

--- app/main.py
+++ app/main.py
@@ -19,17 +19,12 @@ def main(**kwargs):
     # net["h2"].cmd("ip route add default via 10.0.3.4")
     # net["h3"].cmd("ip route add default via 10.0.2.1")
 
-    # net["r1"].cmd("ip route add to 10.0.0.0 via 10.0.0.1 dev r1-eth1")
-    # net["r1"].cmd("ip route add to 10.0.3.0 via 10.0.3.2 dev r1-eth2")
-
-    net["r1"].cmd("ip route add to 10.0.1.0/24 via 10.0.1.2 dev r1-eth3")
+    net["r1"].cmd("ip route add to 10.0.1.0/24 dev r1-eth3")
     net["r2"].cmd("ip route add to 10.0.1.0/24 via 10.0.1.1 dev r2-eth1")
 
     # net["r2"].cmd("ip route add to 10.0.2.0 via 10.0.2.2 dev r2-eth2")
 
     net["r1"].cmd("ip route add to 10.0.2.0/24 via 10.0.1.2 dev r1-eth3")
-
-    net["h1"].cmd("ip route add to 10.0.3.0/24 via 10.0.0.3 dev h1-eth1")
 
     CLI(net)
     input("Network running. Press Enter to stop...")
